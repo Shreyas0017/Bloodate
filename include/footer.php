4eca3f7a1fb593bba2f5baee86f64041c18db732
Shreyas Saha <shreyassaha00@gmail.com>
2025-03-09 16:54:47 +0530

    Update footer.php

--- include/footer.php
+++ include/footer.php
@@ -5,11 +5,6 @@
 						<h1 class="text-center">Contact Us</h1>
 						<hr class="white-bar">
 
-							<h1 class="text-center">
-								<a  target="_blank" href="https://www.facebook.com/exceptionalprogrammers"><i style="color: #fff;" class="fab fa-facebook-square"></i></a>
-
-								<a target="_blank" href="https://www.youtube.com/exceptionalprogrammers"><i style="color: #fff;" class="fab fa-youtube-square"></i></a> 
-							</h1>
 
 							<div class="details">
 								 <p> 7439361373 <i class="fa fa-phone"></i>
